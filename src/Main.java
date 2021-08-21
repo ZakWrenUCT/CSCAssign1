4d8031746e45daac1f1d763039cd76bda1ad0e8c
Zak <zakwrn@gmail.com>
2021-08-21 15:15:19 +0200

    first

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main{
+
+    public static void main(String[] args) {
+        //take user input of :file name, filter size (odd int), output name
+	    //get data set
+        //one-dimensional where all data sets are odd with between 3 to 21 items.
+
+        //parse data (space between pos and val)
+        //data is float between 0 and 5
+
+        //implement median filter of size x and output array
+        //algorithm in size = 3: pass first element and last, and for all other values use median of the data point below and above
+        //so border = size/2
+        //output to same format as input and correct up to 5 dec places
+
+        //use fork/join framework to do this in parallel (divide and concur)
+        //must try different # of threads
+        //use System.nanoTime() or System.currentTImeMillis() to measure
+        //System.gc() after timing block
+        int[] array = {1,5,10,1,0};
+        int low = 0;
+        int high = array.length;
+        ComputeMedian cm = new ComputeMedian(array, low, high);
+        cm.compute();
+        //System.out.println("a");
+        //compare run times of parallel to serial
+        //System.out.println(5/2);
+    }
+
+}
