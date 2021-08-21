90ad0076175c53c7eb2e7d75ff9a8b27fabed8a8
Zak <zakwrn@gmail.com>
2021-08-21 19:30:07 +0200

    Parallel works, implemented text parsing, sequential class broken

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+//import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+//import static java.lang.Integer.valueOf;
+//import static java.lang.Float.*;
 
 public class Main{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         //take user input of :file name, filter size (odd int), output name
 	    //get data set
         //one-dimensional where all data sets are odd with between 3 to 21 items.
@@ -19,15 +27,29 @@ public class Main{
         //must try different # of threads
         //use System.nanoTime() or System.currentTImeMillis() to measure
         //System.gc() after timing block
-        float[] array = {1,5,10,2,3,2};
-        int low = 0;
-        int high = array.length;
+
+        //float[] array = {1,5,10,2,3,2,9,15};
+        String name = "sampleInput100";
+        File text = new File(name+".txt");
+        Scanner scan = new Scanner(text);
+
+
+        int lineNumber = 1;
+        String line = scan.nextLine();
+        int high = parseInt(line);
+
+        float[] input = new float[high];
+        while(scan.hasNextLine()){
+            line = scan.nextLine();
+            input[lineNumber-1]= Float.parseFloat(line.split(" ")[1].replace(',','.'));
+            lineNumber++;
+        }
+
+            //float[] array = {0,1,2,3};
         int size = 3;
-        ProcessArray cm = new ProcessArray(array, low, high, size);
+        ProcessArray cm = new ProcessArray(input, 0, input.length, size);
         System.out.print(Arrays.toString(cm.compute()));
-        //System.out.println("a");
         //compare run times of parallel to serial
-        //System.out.println(5/2);
     }
 
 }
