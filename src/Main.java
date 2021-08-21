36f44eb9c589b36b26202af1be64959d7bef9618
Zak <zakwrn@gmail.com>
2021-08-21 21:46:05 +0200

    WIP
    -Implemented sequential class, performance timing
    -parallel too slow

--- src/Main.java
+++ src/Main.java
@@ -11,28 +11,15 @@ import static java.lang.Integer.parseInt;
 public class Main{
 
     public static void main(String[] args) throws FileNotFoundException {
-        //take user input of :file name, filter size (odd int), output name
-	    //get data set
-        //one-dimensional where all data sets are odd with between 3 to 21 items.
-
-        //parse data (space between pos and val)
-        //data is float between 0 and 5
-
-        //implement median filter of size x and output array
-        //algorithm in size = 3: pass first element and last, and for all other values use median of the data point below and above
-        //so border = size/2
-        //output to same format as input and correct up to 5 dec places
-
-        //use fork/join framework to do this in parallel (divide and concur)
         //must try different # of threads
-        //use System.nanoTime() or System.currentTImeMillis() to measure
+        //use System.nanoTime() or System.currentTimeMillis() to measure
         //System.gc() after timing block
 
-        //float[] array = {1,5,10,2,3,2,9,15};
+        //float[] input = {1,5,10,2,3,2,9,15,1};
+
         String name = "sampleInput100";
         File text = new File(name+".txt");
         Scanner scan = new Scanner(text);
-
 
         int lineNumber = 1;
         String line = scan.nextLine();
@@ -45,10 +32,39 @@ public class Main{
             lineNumber++;
         }
 
-            //float[] array = {0,1,2,3};
         int size = 3;
-        ProcessArray cm = new ProcessArray(input, 0, input.length, size);
-        System.out.print(Arrays.toString(cm.compute()));
+        ProcessArray cm = new ProcessArray(input, 0, input.length, size, 12);
+        ProcessArraySeq cmseq = new ProcessArraySeq(input, 0, input.length, size);
+
+
+
+        System.gc();
+
+        long seqStart = System.nanoTime();
+        float[] seq = cm.compute();
+        long seqEnd = System.nanoTime();
+
+        System.gc();
+
+        long paraStart = System.nanoTime();
+        float[] parallel = cm.compute();
+        long paraEnd = System.nanoTime();
+
+/*        long paraStart = System.currentTimeMillis();
+        float[] parallel = cm.compute();
+        long paraEnd = System.currentTimeMillis();
+
+        long seqStart = System.currentTimeMillis();
+        float[] seq = cm.compute();
+        long seqEnd = System.currentTimeMillis();*/
+
+
+        System.out.println(paraEnd-paraStart);
+        System.out.println(seqEnd-seqStart);
+
+        //System.out.print(Arrays.toString(cm.compute()));
+        //System.out.print(Arrays.toString(cmseq.calculate()));
+        System.out.print(Arrays.equals(cm.compute(),cmseq.calculate()));
         //compare run times of parallel to serial
     }
 
--- src/ProcessArraySeq.java
+++ src/ProcessArraySeq.java
@@ -5,7 +5,6 @@ import java.util.concurrent.RecursiveTask;
 
 class ProcessArraySeq {
     int lo; int hi; float[] arr; int si; float[] out;
-
     ProcessArraySeq(float[] a, int l, int h, int s) {
         arr=a;
         lo=l;
@@ -13,30 +12,29 @@ class ProcessArraySeq {
         si=s;
         //seqThresh=a.length-2;
     }
-
-        out = Arrays.copyOfRange(arr, lo ,hi );
+    float[] calculate() {
+        out = Arrays.copyOfRange(arr, lo, hi);
         float[] subset;
-            for (int i = lo; i < hi; i++){
-                //logic here
-                if (i == 0 || i == arr.length - 1) {
-                    out[i-lo] = arr[i];
-                } else {
-                    subset = Arrays.copyOfRange(arr, i - si/2, i + (si/2)+1);
-                    //System.out.println(subset.toString());
-                    out[i-lo] = findMedian(subset);
-                }
-            System.out.println(Arrays.toString(out));
-            return out;
-
-    }
-
-        public static float findMedian(float[] ar)
-        {
-            //sort the array
-            Arrays.sort(ar);
-            //even case
-            if (ar.length % 2 != 0)
-                return ar[ar.length / 2];
-            return (float) ((ar[(ar.length - 1) / 2] + ar[ar.length / 2]) / 2.0);
+        for (int i = lo; i < hi; i++) {
+            //logic here
+            if (i == 0 || i == arr.length - 1) {
+                out[i - lo] = arr[i];
+            } else {
+                subset = Arrays.copyOfRange(arr, i - si / 2, i + (si / 2) + 1);
+                out[i - lo] = findMedian(subset);
+            }
         }
+        System.out.println(Arrays.toString(out));
+        return out;
     }
+
+    public static float findMedian(float[] ar)
+    {
+        //sort the array
+        Arrays.sort(ar);
+        //even case
+        if (ar.length % 2 != 0)
+            return ar[ar.length / 2];
+        return (float) ((ar[(ar.length - 1) / 2] + ar[ar.length / 2]) / 2.0);
+    }
+}
